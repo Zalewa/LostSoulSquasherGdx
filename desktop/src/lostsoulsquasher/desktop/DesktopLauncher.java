d20425139f49767818a5bfec26ce9920407d338a
Zalewa <zalewapl@gmail.com>
2014-12-26 22:59:16 +0100

    Initialized LibGDX project and copied assets over from .NET version.

---
+++ desktop/src/lostsoulsquasher/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package lostsoulsquasher.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import lostsoulsquasher.LostSoulGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LostSoulGame(), config);
+	}
+}
